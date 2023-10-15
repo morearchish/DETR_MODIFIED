a6dc8f653eb7f87097d0f6ec15e61e2dc44a6752
Archish <archish28022001@gmail.com>
2023-10-15 21:21:55 +0530

    main

--- models/backbone.py
+++ models/backbone.py
@@ -29,9 +29,10 @@ class BackboneBase(nn.Module):
             if not train_backbone or 'layer2' not in name and 'layer3' not in name and 'layer4' not in name:
                 parameter.requires_grad_(False)
         if return_interm_layers:
-            return_layers = {"layer1": "0", "layer2": "1", "layer3": "2", "layer4": "3"}
+            return_layers = {'block1': '0', 'block2': '1', 'block3': '2', 'block4': '3'}
+
         else:
-            return_layers = {'layer4': "0"}
+            return_layers = {'block4': "0"}
         self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
         self.num_channels = num_channels
 
@@ -59,38 +60,6 @@ class Backbone(BackboneBase):
 
 
 
-class BackboneBase(nn.Module):
-
-    def __init__(self, backbone: nn.Module, train_backbone: bool, num_channels: int, return_interm_layers: bool):
-        super().__init__()
-        for name, parameter in backbone.named_parameters():
-            if not train_backbone:
-                parameter.requires_grad_(False)
-        if return_interm_layers:
-            return_layers = {"layer1": "0", "layer2": "1", "layer3": "2", "layer4": "3"}
-        else:
-            return_layers = {'layer4': "0"}
-        self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
-        self.num_channels = num_channels
-
-    def forward(self, tensor_list: NestedTensor):
-        xs = self.body(tensor_list.tensors)
-        out: Dict[str, NestedTensor] = {}
-        for name, x in xs.items():
-            m = tensor_list.mask
-            assert m is not None
-            mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
-            out[name] = NestedTensor(x, mask)
-        return out
-
-class Backbone(BackboneBase):
-
-    def __init__(self, name: str, train_backbone: bool, return_interm_layers: bool, dilation: bool):
-        model_name = 'google/vit-base-patch16-224'
-        model = ViTForImageClassification.from_pretrained(model_name, pretrained=False)
-
-        num_channels = model.config.hidden_size
-        super().__init__(model, train_backbone, num_channels, return_interm_layers)
 
 
 
